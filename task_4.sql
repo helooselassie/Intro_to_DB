fdc58da536462f209f71e551af5c3ea66dcd70d9
helooselassie <selassieheloo@gmail.com>
2024-10-21 13:41:10 +0000

    Initial commit

--- task_4.sql
+++ task_4.sql
@@ -1,5 +1,9 @@
--- Use the database passed as an argument
-USE alx_book_store;
-
 -- Get full description of the 'books' table
-SHOW COLUMNS FROM books;
+SELECT 
+    COLUMN_NAME,
+    COLUMN_TYPE 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'books';
